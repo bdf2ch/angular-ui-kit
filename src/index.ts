c90f4f0396ecc78dab8350849321a34e5cdaede8
bdf2ch <bdf2ch@gmail.com>
2017-05-03 20:34:25 +0300

    tree added

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { TextOverflowDirective } from './text-overflow/text-overflow.directive';
+export { TreeComponent } from './tree/tree.component';
+export { TreeItemComponent } from './tree/tree-item.component';
+export { TreeService } from './tree/tree.service';
 export { UiKitModule } from './ui-kit.module';
--- src/ui-kit.module.ts
+++ src/ui-kit.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { TextOverflowDirective } from './text-overflow/text-overflow.directive';
+import { TreeComponent } from "./tree/tree.component";
+import { TreeService } from "./tree/tree.service";
 
 @NgModule({
     imports: [],
     declarations: [
-        TextOverflowDirective
+        TextOverflowDirective,
+        TreeComponent
     ],
     exports: [
         TextOverflowDirective
     ],
-    providers: []
+    providers: [
+        TreeService
+    ]
 })
 export class UiKitModule {};
