0b011f615665559a24eb04afcd67e2fb442e2aa0
bdf2ch <bdf2ch@gmail.com>
2017-05-06 21:01:26 +0300

    TreeItem anf TreeItemConfig added to exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { TextOverflowDirective } from './text-overflow/text-overflow.directive';
 export { TreeComponent } from './tree/tree.component';
 export { TreeItemComponent } from './tree/tree-item.component';
 export { TreeService } from './tree/tree.service';
+export { TreeItem, TreeItemConfig } from './tree/tree-item';
 export { ModalComponent, ModalContentComponent } from './modal/modal.component';
 export { ModalService } from './modal/modal.service';
 export { UiKitModule } from './ui-kit.module';
