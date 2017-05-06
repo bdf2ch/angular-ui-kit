2e7608088f229ac043e6c399c152dd3236a6fa65
bdf2ch <bdf2ch@gmail.com>
2017-05-06 20:46:19 +0300

    modal exports added

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export { TextOverflowDirective } from './text-overflow/text-overflow.directive';
 export { TreeComponent } from './tree/tree.component';
 export { TreeItemComponent } from './tree/tree-item.component';
 export { TreeService } from './tree/tree.service';
+export { ModalComponent, ModalContentComponent } from './modal/modal.component';
+export { ModalService } from './modal/modal.service';
 export { UiKitModule } from './ui-kit.module';
