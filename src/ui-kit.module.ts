f47cb33ca02479183441ec0b7cef148393db9551
bdf2ch <bdf2ch@gmail.com>
2017-05-06 21:16:11 +0300

    modal added

--- src/ui-kit.module.ts
+++ src/ui-kit.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { TextOverflowDirective } from './text-overflow/text-overflow.directive';
 import { TreeComponent } from "./tree/tree.component";
+import { TreeItemComponent } from "./tree/tree-item.component";
 import { TreeService } from "./tree/tree.service";
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from './modal/modal.service';
@@ -10,10 +11,13 @@ import { ModalService } from './modal/modal.service';
     declarations: [
         TextOverflowDirective,
         TreeComponent,
+        TreeItemComponent,
         ModalComponent
     ],
     exports: [
         TextOverflowDirective,
+        TreeComponent,
+        TreeItemComponent,
         ModalComponent
     ],
     providers: [
